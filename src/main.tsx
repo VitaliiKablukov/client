d1c2d361d348d04eaf5a3413fcfc44579a57302b
VitaliiKablukov <vetal.kablukov98@gmail.com>
2023-12-07 00:07:44 +0200

    add basname main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<Provider store={store}>
-		<BrowserRouter basename="/client/">
+		<BrowserRouter basename="/client">
 			<App />
 			<ToastContainer position="bottom-left" autoClose={2000} />
 		</BrowserRouter>
