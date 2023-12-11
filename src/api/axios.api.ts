45a016b3c442e6b8a0fdc71bdb7d5955b61c424d
VitaliiKablukov <VitaliiKablukov@users.noreply.github.com>
2023-12-11 02:58:36 +0000

    deploy: 702a0f9621d009a5467bb41ff9da199a522bb40d

--- src/api/axios.api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-import { getTokenFromLocalStorage } from '../helpers/localstorage.helper'
-export const instance = axios.create({
-	baseURL: 'https://spa-jn0u.onrender.com/api',
-	headers: {
-		Authorization: 'Bearer ' + getTokenFromLocalStorage() || '',
-	},
-})
